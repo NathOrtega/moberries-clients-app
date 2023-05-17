7f0d8ed727c3d47b945dfb11b7f1996a68e30d1d
Nataly Ortega <nath_0563@hotmail.com>
2023-05-17 20:20:13 +0200

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { up } from "styled-breakpoints";
+import MoBerriesLogo from "../assets/MoBerriesLogo";
+
+const StyledHeader = styled.div`
+	width: 100%;
+	height: 136px;
+	padding: 36px 24px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background-color: #011627;
+
+	${up("md")} {
+		height: 160px;
+		padding: 42px 40px;
+	}
+
+	${up("xl")} {
+		height: 162px;
+		padding: 45px 165px;
+	}
+`;
+
+export default function Header() {
+	return (
+		<StyledHeader>
+			<MoBerriesLogo style={{maxHeight: "76px"}} />
+		</StyledHeader>
+	);
+}
